2e4a1808aef0bbe05c6b345d015f6a340ae4f481
erbdevs <erb.devs@gmail.com>
2024-09-11 12:40:33 +0200

    2023 03 gear ratios; part_1 completed

--- 2023/03_gear_ratios/part_1/file_parser_spec.rb
+++ 2023/03_gear_ratios/part_1/file_parser_spec.rb
@@ -5,13 +5,23 @@ RSpec.describe FileParser do
     subject(:parsed_lines) { described_class.new(file_content).parse }
     
     context "basic" do
-      let(:file_content) { "467.#.114.."}
+      let(:file_content) { "467.#.114."}
 
       it do
-        expect(parsed_lines).to eq(
-          {
-            0 => { 0 => 467, 4 => "#", 6 => 114} 
-          }
+        symbols = parsed_lines[0]
+        symbols_summary = symbols.map { [_1.value, _1.y, _1.x] }
+        expect(symbols_summary).to eq(
+          [["#", 0, 4]]
+        )
+
+        numbers = parsed_lines[1]
+        numbers_summary = numbers.map { [_1.value, _1.y, _1.x_start, _1.x_end] }
+        expect(numbers_summary).to eq(
+          [
+            # value | y | x_start | x_end
+            [    467,  0,        0,     2 ],
+            [    114,  0,        6,     8 ],
+          ]
         )
       end
     end
@@ -33,19 +43,73 @@ RSpec.describe FileParser do
       end
 
       it do
-        expect(parsed_lines).to eq(
-          {
-            0 => { 0 => 467, 5 => 114 },
-            1 => { 3 => "*" },
-            2 => { 2 => 35, 6 => 633 },
-            3 => { 6 => "#" },
-            4 => { 0 => 617, 3 => "*" },
-            5 => { 5 => "+", 7 => 58 },
-            6 => { 2 => 592 },
-            7 => { 6 => 755 },
-            8 => { 3 => "$", 5 => "*" },
-            9 => { 1 => 664, 5 => 598 },
-          }
+        symbols = parsed_lines[0]
+        symbols_summary = symbols.map { [_1.value, _1.y, _1.x] }
+        expect(symbols_summary).to eq(
+          [
+            ["*", 1, 3],
+            ["#", 3, 6],
+            ["*", 4, 3],
+            ["+", 5, 5],
+            ["$", 8, 3],
+            ["*", 8, 5],
+          ]
+        )
+
+        numbers = parsed_lines[1]
+        numbers_summary = numbers.map { [_1.value, _1.y, _1.x_start, _1.x_end] }
+        expect(numbers_summary).to eq(
+          [
+            # value | y | x_start | x_end
+            [    467,  0,        0,     2 ],
+            [    114,  0,        5,     7 ],
+            [     35,  2,        2,     3 ],
+            [    633,  2,        6,     8 ],
+            [    617,  4,        0,     2 ],
+            [     58,  5,        7,     8 ],
+            [    592,  6,        2,     4 ],
+            [    755,  7,        6,     8 ],
+            [    664,  9,        1,     3 ],
+            [    598,  9,        5,     7 ],
+          ]
+        )
+      end
+    end
+
+    context "basic" do
+      let(:file_content) do
+        <<~LINE
+          111.1*
+          1*1.1*
+          111.1*
+        LINE
+      end
+
+      it do
+        symbols = parsed_lines[0]
+        symbols_summary = symbols.map { [_1.value, _1.y, _1.x] }
+        expect(symbols_summary).to eq(
+          [
+            ["*", 0, 5],
+            ["*", 1, 1],
+            ["*", 1, 5],
+            ["*", 2, 5],
+          ]
+        )
+
+        numbers = parsed_lines[1]
+        numbers_summary = numbers.map { [_1.value, _1.y, _1.x_start, _1.x_end] }
+        expect(numbers_summary).to eq(
+          [
+            # value | y | x_start | x_end
+            [    111,  0,        0,     2 ],
+            [      1,  0,        4,     4 ],
+            [      1,  1,        0,     0 ],
+            [      1,  1,        2,     2 ],
+            [      1,  1,        4,     4 ],
+            [    111,  2,        0,     2 ],
+            [      1,  2,        4,     4 ],
+          ]
         )
       end
     end
